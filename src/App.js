cf01af1172bc8bdaa595fa3687b54a563c3f2ba7
Aidan Dang <aidandang@gmail.com>
2020-04-02 16:00:36 -0700

    updated customhook.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
+import {useFetchData} from "./customHooks"
 import "./App.css";
-
-function useFetchData(arg) {
-  const [data, setData] = useState(null);
-  
-  useEffect(() => {
-    axios.get(arg)
-    .then(response => {
-      // get the data
-      setData(response.data);
-    })
-    .catch(err => {
-      // deal with the error in here
-      console.log("Error: CANNOT get the data!", err);
-    })
-  }, [arg])
-
-  return data 
-}
 
 // The parent component
 function App() {
---
+++ src/customHooks.js
@@ -0,0 +1,20 @@
+import {useState, useEffect} from "react";
+import axios from 'axios';
+
+export function useFetchData(arg) {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    axios.get(arg)
+    .then(response => {
+      // get the data
+      setData(response.data);
+    })
+    .catch(err => {
+      // deal with the error in here
+      console.log("Error: CANNOT get the data!", err);
+    })
+  }, [arg])
+
+  return data 
+}
