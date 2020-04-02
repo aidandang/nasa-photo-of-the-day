ef208ee370d218066606db117456ec2a58bab50a
Aidan Dang <aidandang@gmail.com>
2020-04-02 15:48:42 -0700

    added a custom hook to fetch data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
+function useFetchData(arg) {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    axios.get(arg)
+    .then(response => {
+      // get the data
+      setData(response.data);
+    })
+    .catch(err => {
+      // deal with the error in here
+      console.log("Error: CANNOT get the data!", err);
+    })
+  }, [arg])
+
+  return data 
+}
+
+// The parent component
 function App() {
+  const apiAddress = 'https://api.nasa.gov/planetary/apod?api_key=R9dCmyw3hwyS2n9ubgAbWnecZpFf6fUZEDkK8Zdm';
+  const data = useFetchData(apiAddress);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {data && <img src={data.url} alt={data.title} />}
     </div>
   );
 }
