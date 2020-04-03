29bc57dd6451e19c5403c8edc8a4fdc2b078ec3d
Aidan Dang <aidandang@gmail.com>
2020-04-02 22:43:57 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import {useFetchData} from "./customHooks"
 import "./App.css";
 
+// A child component to display the image
+function DisplayImg({src, alt}) {
+  return (
+    <img src={src} alt={alt} />
+  )
+}
+
 // The parent component
 function App() {
-  const apiAddress = 'https://api.nasa.gov/planetary/apod?api_key=R9dCmyw3hwyS2n9ubgAbWnecZpFf6fUZEDkK8Zdm';
+  const apiAddress = 'https://api.nasa.gov/planetary/apod?api_key=R9dCmyw3hwyS2n9ubgAbWnecZpFf6fUZEDkK8Zdm&date=2017-07-09';
   const data = useFetchData(apiAddress);
 
   return (
     <div className="App">
-      {data && <img src={data.url} alt={data.title} />}
+      {!data ? <p>Loading...</p> : <DisplayImg src={data.hdurl} alt={data.title} />}
     </div>
   );
 }
